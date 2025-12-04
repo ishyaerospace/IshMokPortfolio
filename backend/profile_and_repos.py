55e00ac03e4c11e8a1d851e8fb629aaf6d7a7d7c
ishyAerospace <119079770+ishyaerospace@users.noreply.github.com>
2025-12-04 11:53:49 +0000

    cache successful

--- backend/profile_and_repos.py
+++ backend/profile_and_repos.py
@@ -14,42 +14,6 @@ def save_cache(data):
     with open(CACHE_FILE, "w") as file:
         json.dump(data, file, indent=4)
 
-"""
-@app.get("/github/profile")
-async def get_github_profile():
-    profile = requests.get(profile_url).json()
-
-    cache =  {
-        "fetched_at": time(),
-        "profile":{
-            "login": profile["login"],
-            "bio": profile["bio"],
-            "avatar": profile["avatar_url"],
-            "public_repos": profile["public_repos"]
-        },
-    }
-    
-    save_cache(cache)
-    return cache
-
-@app.get("/github/repos")
-async def get_github_repos():
-    repos = requests.get(repos_url).json()
-
-    cache = {
-        "repos":[ 
-        {
-            "name": repo["name"],
-            "description": repo["description"],
-            "url": repo["html_url"]
-        }
-        for repo in repos
-        ],
-    }
-
-    save_cache(cache)
-    return cache
-"""
 @app.get("/")
 async def get_github_info():
     profile = requests.get(profile_url).json()
