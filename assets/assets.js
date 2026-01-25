8a5e3c0bb29c7216365c410c80611406d9db5420
ishyAerospace <119079770+ishyaerospace@users.noreply.github.com>
2026-01-25 21:02:02 +0000

    ran npm 'audit fix --force' to patch NEXT critical vulnerability

--- assets/assets.js
+++ assets/assets.js
@@ -49,5 +49,5 @@ export const assets = {
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js, python, C++, C#' },
     
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built multiple projects' }
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'click to see my work' }
 ];
